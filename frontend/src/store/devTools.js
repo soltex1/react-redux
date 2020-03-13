ce9e148e315e5969eac9a21bff1508aed98fafe0
Ruben Santos <ruben.d.santos@tecnico.ulisboa.pt>
2020-03-13 14:38:08 +0000

    Add redux-thunk to the store configuration

--- frontend/src/store/devTools.js
+++ frontend/src/store/devTools.js
@@ -1,10 +1,10 @@
 import { applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
 
 const devTools =
   process.env.NODE_ENV === 'production'
-    ? applyMiddleware()
-    : composeWithDevTools(applyMiddleware());
-
+    ? applyMiddleware(thunk)
+    : composeWithDevTools(applyMiddleware(thunk))
 
 export default devTools
